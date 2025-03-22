f51529210d3f0a024b2bbbc40f05535fafe510ed
Hermes <hermeschigoziri@gmail.com>
2025-03-22 16:41:18 +0100

    create a class for managing ktlint

---
+++ ktlint-plugin/plugin/src/main/kotlin/com/github/truepadawan/ktlint/problems/invoker/KtLintInvoker.kt
@@ -0,0 +1,44 @@
+package com.github.truepadawan.ktlint.problems.invoker
+
+import com.pinterest.ktlint.cli.ruleset.core.api.RuleSetProviderV3
+import com.pinterest.ktlint.rule.engine.api.Code
+import com.pinterest.ktlint.rule.engine.api.KtLintRuleEngine
+import com.pinterest.ktlint.rule.engine.api.LintError
+import com.pinterest.ktlint.rule.engine.core.api.RuleProvider
+import com.pinterest.ktlint.ruleset.standard.rules.IndentationRule
+import java.io.File
+import java.util.ServiceConfigurationError
+import java.util.ServiceLoader
+
+class KtLintInvoker(private val engine: KtLintRuleEngine) {
+    companion object Factory {
+        fun initialize(): KtLintInvoker {
+            val ruleProviders = loadRuleSetsFromClassPath()
+            val engine = KtLintRuleEngine(ruleProviders = ruleProviders)
+            return KtLintInvoker(engine)
+        }
+
+        private fun loadRuleSetsFromClassPath(): Set<RuleProvider> {
+            try {
+                return ServiceLoader.load(RuleSetProviderV3::class.java).flatMap {
+                    it.getRuleProviders()
+                }.toSet()
+            } catch (e: ServiceConfigurationError) {
+                println("Error while loading rulesets")
+                println(e.printStackTrace())
+                return emptySet()
+            }
+        }
+    }
+
+    fun invokeLinter(file: File): LintErrorResult {
+        println("Linting file: ${file.name}")
+        val errors = mutableListOf<LintError>()
+        engine.lint(Code.fromFile(file)) { lintError: LintError ->
+            errors.add(lintError)
+        }
+        return LintErrorResult(file, errors)
+    }
+}
+
+data class LintErrorResult(val file: File, val errors: List<LintError>)
