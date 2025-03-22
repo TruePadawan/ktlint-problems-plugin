713656c7271e3feeef725a7d75255ac1a360a5d3
Hermes <hermeschigoziri@gmail.com>
2025-03-22 16:40:35 +0100

    inject the Problems API service into the plugin

--- ktlint-plugin/plugin/src/main/kotlin/com/github/truepadawan/ktlint/problems/KtlintProblemsPlugin.kt
+++ ktlint-plugin/plugin/src/main/kotlin/com/github/truepadawan/ktlint/problems/KtlintProblemsPlugin.kt
@@ -3,8 +3,23 @@ package com.github.truepadawan.ktlint.problems
 import com.github.truepadawan.ktlint.problems.tasks.KtLintCheckTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.problems.ProblemGroup
+import org.gradle.api.problems.ProblemReporter
+import org.gradle.api.problems.Problems
+import javax.inject.Inject
 
 class KtlintProblemsPlugin : Plugin<Project> {
+    companion object {
+        val PROBLEM_GROUP = ProblemGroup.create("ktlintProblems", "KtLint Problems Plugin")
+    }
+
+    private val problemReporter: ProblemReporter
+
+    @Inject
+    constructor(problems: Problems) {
+        problemReporter = problems.reporter
+    }
+
     override fun apply(project: Project) {
         project.tasks.register("ktlintCheck", KtLintCheckTask::class.java) { task ->
             task.description = "Run KtLint on all kotlin files in the project and report issues via the Problems API"
